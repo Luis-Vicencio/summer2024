008defba83a80d0948b12d4e9c9b72ebeacacd70
Luis-Vicencio <luis.vicencio02@utrgv.edu>
2024-07-12 21:21:48 +0000

    setup done

---
+++ my_first_program/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello, Luis Vicencio! To the Moon");
+}
---
+++ my_first_program/target/release/my_first_program.d
@@ -0,0 +1,1 @@
+/workspaces/summer2024/my_first_program/target/release/my_first_program: /workspaces/summer2024/my_first_program/src/main.rs
